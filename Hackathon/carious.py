3bd24d3d2c16a0e6ea71d50929f9a80edd3b521b
deolishus <46029748+deolishus@users.noreply.github.com>
2019-04-04 09:24:02 -0700

    Add files via upload
    First commit.

---
+++ Hackathon/carious.py
@@ -0,0 +1,76 @@
+import pygame, sys
+import pygame.event as keyboard
+from time import sleep
+
+pygame.init()
+
+size = width, height = 1080, 720
+screen = pygame.display.set_mode(size)
+
+#picture locations
+car = "/home/deo/Desktop/Hackathon/city/car.jpg"
+food = "/home/deo/Desktop/Hackathon/city/food.jpg"
+stanford = "/home/deo/Desktop/Hackathon/city/stanford.jpg"
+events = "/home/deo/Desktop/Hackathon/city/events.jpg"
+
+#variables to load pictures with resize function to scale to window
+firstpic = pygame.transform.scale(pygame.image.load(car), size)
+secondpic = pygame.transform.scale(pygame.image.load(food), size)
+thirdpic = pygame.transform.scale(pygame.image.load(stanford), size)
+fourthpic = pygame.transform.scale(pygame.image.load(events), size)
+
+def quit_screen():
+    pygame.quit()
+    sys.exit()
+
+def fourth_screen():
+    while True:
+        screen.blit(fourthpic, (0,0))
+        pygame.display.update()
+        for event in keyboard.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    third_screen()
+
+def third_screen():
+    while True:
+        screen.blit(thirdpic, (0,0))
+        pygame.display.update()
+        for event in keyboard.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    second_screen()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    fourth_screen()
+
+
+
+def second_screen():
+    while True:
+        screen.blit(secondpic, (0,0))
+        pygame.display.update()
+        for event in keyboard.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    first_screen()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    third_screen()
+
+
+def first_screen():
+    while True:
+        screen.blit(firstpic, (0,0))
+        pygame.display.update()
+        for event in keyboard.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    second_screen()
+
+while True:
+    for event in keyboard.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                quit_screen()
+    first_screen()
